2cb3c5c6a2942c643a3984ef2fe51762883000bb
Josue-MNK <benjamingiron1@gmail.com>
2018-07-25 17:36:27 -0600

    actualizada

--- main.go
+++ main.go
@@ -14,24 +14,24 @@ type sumando struct {
 }
 
 func main() {
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/operacion/suma", Suma).Methods("GET")
-	
-
-    log.Fatal(http.ListenAndServe(":3002", router))
+    log.Fatal(http.ListenAndServe(":3001", router))
 }
 
 func Suma(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
 	if err := r.ParseForm(); err != nil {
-		log.Printf("Error parsing form: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		error := map[string]string{"error": "No se ingreso un numero como sumando..."}
+			json.NewEncoder(w).Encode(error)
 		return
 }
 
 	sumA := r.Form.Get("sumandoA")
 	sumandoA, err := strconv.ParseFloat(sumA, 64)
 	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 			error := map[string]string{"error": "No se ingreso un numero como sumando..."}
 			json.NewEncoder(w).Encode(error)
 			return
@@ -40,13 +40,14 @@ func Suma(w http.ResponseWriter, r *http.Request) {
 	sumB := r.Form.Get("sumandoB")
 	sumandoB, err := strconv.ParseFloat(sumB, 64)
 	if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
 			error := map[string]string{"error" : "No se ingreso un numero como sumando..."}
     		json.NewEncoder(w).Encode(error)
 			return
 	}
-
+	
+	w.WriteHeader(http.StatusOK)
 	total := sumandoA + sumandoB;
 	suma := map[string]float64{"total" : total}
     json.NewEncoder(w).Encode(suma)
-
 }
