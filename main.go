432fee74fbea37616aaaa8db47bac86aa36c4ee9
MacBook Pro <benjamingiron1@gmail.com>
2018-07-25 12:52:53 -0600

    Terminada

--- main.go
+++ main.go
@@ -2,26 +2,51 @@ package main
 
 
 import (
-    "fmt"
     "log"
 	"net/http"
-
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"strconv"
 )
+type sumando struct {
+	sumandoA float32
+	sumandoB float32
+}
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/operacion/suma", Suma)
+	router.HandleFunc("/operacion/suma", Suma).Methods("GET")
+	
 
     log.Fatal(http.ListenAndServe(":3002", router))
 }
 
 func Suma(w http.ResponseWriter, r *http.Request) {
-	
 
-	vars := mux.Vars(r)
-	sumandoA := vars["sumandoA"]
-	fmt.Fprintln(w, "Todo show:", sumandoA)
+	if err := r.ParseForm(); err != nil {
+		log.Printf("Error parsing form: %s", err)
+		return
+}
+
+	sumA := r.Form.Get("sumandoA")
+	sumandoA, err := strconv.ParseFloat(sumA, 64)
+	if err != nil {
+			error := map[string]string{"error": "No se ingreso un numero como sumando..."}
+			json.NewEncoder(w).Encode(error)
+			return
+	}
+
+	sumB := r.Form.Get("sumandoB")
+	sumandoB, err := strconv.ParseFloat(sumB, 64)
+	if err != nil {
+			error := map[string]string{"error" : "No se ingreso un numero como sumando..."}
+    		json.NewEncoder(w).Encode(error)
+			return
+	}
+
+	total := sumandoA + sumandoB;
+	suma := map[string]float64{"total" : total}
+    json.NewEncoder(w).Encode(suma)
 
 }
